53ea7ca43af647f696340935a7b010ab6b726bb0
swisscheez2 <ebriguet@yahoo.com>
2023-12-06 22:37:04 +0100

    can choose voting option
    to do fix voting but last comment doesn't get a vote. and first one only gets one on second iteration.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-
+import threading
 import time
 import random
 
@@ -31,15 +31,14 @@ def handle_cookie_banner(driver):
         accept_button.click()
     except:
         pass
+
 def has_comments(url):
     print(f"Checking for comments in article: {url}")
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-
     with open('output.html', 'w', encoding='utf-8') as file:
         file.write(response.text)
-
 
     comment_section = soup.find('div', id='commentSection')
     if comment_section:
@@ -49,7 +48,7 @@ def has_comments(url):
         print("No comment section found.")
         return False
 
-def scrape_comments(driver, url):
+def scrape_comments(driver, url, vote_option_index):
     driver.get(url)
     print(url)
 
@@ -67,7 +66,7 @@ def scrape_comments(driver, url):
             print("No comments found.")
             return
 
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 4).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'article[data-testid="CommentCard"]'))
         )
     except TimeoutError:
@@ -75,7 +74,16 @@ def scrape_comments(driver, url):
         return
 
     comments = driver.find_elements(By.CSS_SELECTOR, 'article[data-testid="CommentCard"]')
+    counter = 0
     for comment in comments:
+        counter += 1
+        if counter < 2:
+            continue
+
+
+
+        delay_ms = random.randint(0, 250)
+        time.sleep(delay_ms / 1000)  # we don't want to poke the bear
         author = comment.find_element(By.CSS_SELECTOR, 'p.authorNickname').text
         timestamp = comment.find_element(By.CSS_SELECTOR, 'p.createdAt').text
         comment_text = comment.find_element(By.CSS_SELECTOR, 'div.jPXCsY').text
@@ -84,37 +92,44 @@ def scrape_comments(driver, url):
         print(f"Comment: {comment_text}")
         print("-" * 40)
 
-        delay_ms = random.randint(0, 250)
-        time.sleep(delay_ms / 1000)  # we don't want to poke the bear
-        # Find and hover over the commentReaction_container element
+        # Find and hover over the commentReaction_container element before we can vote
         comment_reaction_container = comment.find_element(By.CSS_SELECTOR, '[class^="commentReaction_container"]')
         actions = ActionChains(driver)
         actions.move_to_element(comment_reaction_container).perform()
-
-        # Wait for the additional details to appear (using a wildcard selector)
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '[class^="commentReactionButton_button"]'))
         )
 
-        # Locate and click on the desired voting button (using a wildcard selector)
-        voting_option = comment.find_element(By.CSS_SELECTOR, '[class^="commentReactionButton_button"]')
-        voting_option.click()
+        # Find and click the specific voting option button based on the index
+        voting_options = comment.find_elements(By.CSS_SELECTOR, '[class^="commentReactionButton_button"]')
+        if vote_option_index < len(voting_options):
+            voting_option = voting_options[vote_option_index]
+            voting_option.click()
 
-main_url = 'https://www.20min.ch'
-article_links = find_article_links(main_url)
+def thread_function(url, options, vote_option_index):
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    scrape_comments(driver, url, vote_option_index)
+    driver.quit()
+
+#main_url = 'https://www.20min.ch'
+#article_links = find_article_links(main_url)
 options = Options()
 options.add_argument("incognito")
 options.add_argument("headless")  # headless
-# lazy init
-driver = webdriver.Chrome(options=options)
 
-for link in article_links:
-    driver =  webdriver.Chrome( options=options)  # Start a new browser instance
-    #driver.execute_script("window.open('about:blank', 'newWindow', 'width=800,height=600');")
-    #driver.switch_to.window(driver.window_handles[-1])  # Switch to the newly opened window
-    #driver.execute_script("window.open('about:blank', '_blank');")  # Open a new tab
-    #driver.switch_to.window(driver.window_handles[-1])  # Switch to the newly opened tab
-    scrape_comments(driver, "https://www.20min.ch/story/e-vignette-seite-des-bundes-lahmgelegt-186614058610")
-    driver.quit()
+number_of_iterations = 50
+number_of_threads = 20
+specific_article_url = ""
+vote_option_index = 5 # Change this to the index of the voting option you want to select
 
+threads = []
+for _ in range(number_of_threads):
+    for _ in range(number_of_iterations // number_of_threads):
+        thread = threading.Thread(target=thread_function, args=(specific_article_url, options, vote_option_index))
+        threads.append(thread)
+        thread.start()
 
+# Waiting for all threads to complete
+for thread in threads:
+    thread.join()
